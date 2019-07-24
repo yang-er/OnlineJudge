1afac41f8e322507d3a43ca5605cbc51f5fd45e1
小羊 <tlylz99@live.cn>
2019-07-24 21:45:06 +0800

    JudgeWeb主程序集部分清理完成。

---
+++ JudgeWeb/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Text;
+
+namespace JudgeWeb
+{
+    public class Program
+    {
+        public static IWebHost Current { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            (Current = CreateWebHostBuilder(args).Build()).Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ JudgeWeb/Startup.cs
@@ -0,0 +1,144 @@
+﻿using EntityFrameworkCore.Cacheable;
+using JudgeWeb.Data;
+using JudgeWeb.Features;
+using JudgeWeb.Features.Mailing;
+using JudgeWeb.Features.OjUpdate;
+using JudgeWeb.Features.Storage;
+using Microsoft.AspNetCore.Authentication.QQ;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace JudgeWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            Environment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 2;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.@";
+
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(150);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/account/sign/login";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/account/sign/access-denied";
+                options.SlidingExpiration = true;
+            });
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection"))
+                       .UseSecondLevelCache());
+
+            services.AddIdentity<User, IdentityRole<int>>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddUserManager<UserManager>()
+                .AddDefaultTokenProviders();
+
+            services.AddSingleton(new ConfigurationBasicAuthorizationService(Configuration));
+
+            services.AddAuthentication().AddQQ(qqOptions =>
+            {
+                qqOptions.AppId = Configuration["Authentication:QQ:AppId"];
+                qqOptions.AppKey = Configuration["Authentication:QQ:AppKey"];
+            });
+
+            services.AddEmailSender(options =>
+                options.Bind(Configuration.GetSection("SendGrid")));
+
+            services.AddOjUpdateService(confBuilder =>
+                confBuilder.AddJsonFile("ojconfig.json", true));
+
+            services.AddProblemRepository();
+            services.AddMarkdown();
+
+            services.AddMvc()
+                .SetTokenTransform<SlugifyParameterTransformer>()
+                .EnableContentFileResult()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .UseAreaParts("JudgeWeb.Areas.", new[] { "Misc", "Account", "Api", "Judge" });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else if (Environment.EnvironmentName == "Test")
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseMiddleware<RealIpMiddleware>();
+                app.UseExceptionHandler("/error");
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
